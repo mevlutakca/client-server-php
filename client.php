ccd719bd59da7256e045990f9be61b622fc0d7fd
mevlutakca <akcamevlut@gmail.com>
2017-03-07 03:42:02 +0300

    documentation for code

--- client.php
+++ client.php
@@ -6,8 +6,10 @@ $url = 'http://localhost/server-client/server.php?method=getWheather';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// prepare curl for basic auth information
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+// set user and password for basic auth
 curl_setopt($ch, CURLOPT_USERPWD, 'a:b');
 
 $icerik = curl_exec($ch);
--- server.php
+++ server.php
@@ -1,15 +1,20 @@
 <?php
-error_reporting(1);
-ini_set('display_errors', 'on');
+
 class Server {
 
 	private $users = [];
 	private $statuses = ['status1', 'status2', 'status3', 'status4', 'status5'];
 
+	/**
+	 * Constructor for Server Class
+	 * @param Array $user User Information for access to the methods
+	 */
 	public function __construct($user = null)
 	{
+		// Set default user informations 
 		$this->setDefault();
 		try{
+			// Check user cridentials
 			$this->checkUser($user);
 		}catch(Exception $e){
 			echo 'Message: ' . $e->getMessage();
@@ -18,39 +23,52 @@ class Server {
 
 
 
-
+	/**
+	 * random wheater status
+	 * @return String Wheather status
+	 */
 	public function getStatus()
 	{
+		// generate random integer value beetwen 0 and count of wheater statuses
 		$rand = rand(0, count($this->statuses));
 		return $this->statuses[$rand];
 	}
 	
+	/**
+	 * random Wheather temperature
+	 * @return int Wheather temperature
+	 */
 	public function getWheather()
 	{
+		// generate and returned random integer
 		return rand(-10, 40);
 	}
 	
+	/**
+	 * generate output from data with headers for json output
+	 * @param  Array  $data   data for output
+	 * @param  integer $status status of output
+	 * @return void
+	 */
 	public function response($data, $status = 200) {
+		// set header for cross domain origin
 		header("Access-Control-Allow-Orgin: *");
+		// set all methods can be access this server
         header("Access-Control-Allow-Methods: *");
+        // set output content type
         header("Content-Type: application/json");
-        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
+
+        // prepare data for json output
         if ( ! is_array($data) )
         	$data = array($data);
         echo json_encode($data);
     }
 
-    private function _requestStatus($code) {
-        $status = array(  
-            200 => 'OK',
-            404 => 'Not Found',   
-            405 => 'Method Not Allowed',
-            500 => 'Internal Server Error',
-        ); 
-        return ($status[$code])?$status[$code]:$status[500]; 
-    }
-
-
+    /**
+     * Check user cridentials
+     * @param  Array $user information about login user
+     * @return boolean if user exists return true else throw an Exception
+     */
 	private function checkUser($user = null)
 	{
 
@@ -65,6 +83,9 @@ class Server {
 		throw new Exception('Kullanıcı Hatası');
 	}
 
+	/**
+	 * setting default user information for login cridentials
+	 */
 	private function setDefault()
 	{
 		$this->users = [
@@ -76,17 +97,25 @@ class Server {
 	}
 }
 
+
+// If server request different from GET do not generate any response
 if ( $_SERVER['REQUEST_METHOD'] !== 'GET' )
 	return false;
 
 
-
+// request should come with QUERY_STRING
 $request = $_GET;
+// default method getStatus if not set method for Server Calls
 $method = 'getStatus';
 if ( ! empty($request) && ! empty($_SERVER['QUERY_STRING']) )
 	$method = $request['method'];
 
+// we are using basic authentication for easy login with cridentials
 $data['username'] = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : 'a';
 $data['password'] = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : 'b';
+
+// create a new Class from Server with login informations
 $api = new Server($data);
+
+// generate response with called method
 $api->response($api->$method());
